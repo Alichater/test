e24f00a36fae7786a7c31da01fadfcb7987e154a
alichater <143833265+Alichater@users.noreply.github.com>
2023-09-26 10:33:26 -0400

    progres

--- Menu.java
+++ Menu.java
@@ -1,7 +1,21 @@
-public class test
-{
-    public static void main(String[] args) 
-    {
-        System.out.print("hello");
+
+public class Menu {
+    public static void main(String[] args) {
+        System.out.print("inserer la piece");
+        int[] gagnant = { 5, 12, 34, 56, 63, 75, 89, 92 };
+        int nombre;
+        System.out.println("écrivez un nombre entre 1 et 100: ");
+        //instruction pour saisir la réponse...
+
+        //vérifier la réponse
+        for (int n : gagnant){
+            if (nombre == n) {
+                System.out.print("vous avez gagné");
+                break;
+            }
+        }
+        
+
+        
     }
 }
