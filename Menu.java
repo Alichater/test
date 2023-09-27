512da3d167fce9d1642a93011ad1b2ea5a5f1ec1
alichater <143833265+Alichater@users.noreply.github.com>
2023-09-27 14:08:54 -0400

    progres

--- Menu.java
+++ Menu.java
@@ -1,21 +1,23 @@
+import java.util.Scanner;
 
 public class Menu {
     public static void main(String[] args) {
-        System.out.print("inserer la piece");
+        Scanner scanner = new Scanner(System.console().reader());
+        System.out.println("inserer le montant a parier:");
+        double montant = scanner.nextDouble();
+        
         int[] gagnant = { 5, 12, 34, 56, 63, 75, 89, 92 };
-        int nombre;
-        System.out.println("écrivez un nombre entre 1 et 100: ");
-        //instruction pour saisir la réponse...
-
-        //vérifier la réponse
-        for (int n : gagnant){
+        System.out.println("écrivez un nombre entre 1 et 100:");
+        int nombre = scanner.nextInt();
+        for (int n : gagnant) {
             if (nombre == n) {
-                System.out.print("vous avez gagné");
+                System.out.print("vous avez gagné:");
+                System.out.print(montant * 10);
+            } else {
+                System.out.print("vous avez perdu");
                 break;
             }
         }
-        
 
-        
     }
 }
