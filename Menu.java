e840a4710307f4eb6dcb146b31df4a9973f087b7
alichater <143833265+Alichater@users.noreply.github.com>
2023-09-29 14:13:24 -0400

    vendredi

--- Menu.java
+++ Menu.java
@@ -3,27 +3,35 @@ import java.util.Scanner;
 public class Menu {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.console().reader());
-        System.out.println("inserer le montant a parier:");
-        double montant = scanner.nextDouble();
-        
         int[] gagnant = { 5, 12, 34, 56, 63, 75, 89, 92 };
-        System.out.println("écrivez un nombre entre 1 et 100:");
-        int nombre = scanner.nextInt();
-        boolean gagne = false;
-        for (int n : gagnant) {
-            if (nombre == n) {
-                gagne = true;
-               break; 
+        String restart;
+        do {
+            System.out.println("inserer le montant a parier:");
+            double montant = scanner.nextDouble();
+
+            System.out.println("écrivez un nombre entre 1 et 100:");
+            int nombre = scanner.nextInt();
+            boolean gagne = false;
+            for (int n : gagnant) {
+                if (nombre == n) {
+                    gagne = true;
+                    break;
+                }
             }
-        }
-        if (gagne){
-            System.out.print("vous avez gagné!");
-            System.out.print(montant * 10);
-        } else {
-            System.out.print("vous avez perdu");}
-            System.out.println("voulez vous recommencez");
-           string reponse = scanner.string next();
-        
+            if (gagne) {
+                System.out.print("vous avez gagné!");
+                System.out.print(montant * 10);
+            } else {
+                System.out.print("vous avez perdu");
+            }
+
+            System.out.println("voulez vous recommencez ou quitter le jeu ?");
+            restart = scanner.next();
+            while (!restart.equals("recommencez") && !restart.equals("quitter")){
+                System.out.println("Réponse invalide");
+                restart = scanner.next();
+            }
+        } while (restart.equals("recommencez"));
 
     }
 }
