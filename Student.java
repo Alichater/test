56d86be37d845ca54ede25eb11beb9bc7e9eb57b
alichater <143833265+Alichater@users.noreply.github.com>
2024-01-23 12:50:07 -0500

    mise à jour

---
+++ Student.java
@@ -0,0 +1,15 @@
+public class Student {
+    //information -> ATTRIBUTS
+    String name ;
+    double average ;
+    //comportements -> MÉTHODES
+    //CONSTRUCTEUR 
+    Student(String name){
+        this.name = name;
+        }
+
+        Student(){
+            this("vide");
+            }
+            
+}
---
+++ TestStudent.java
@@ -0,0 +1,10 @@
+public class TestStudent {
+    public static void main(String[]args) {
+        //tester le constructeur
+        Student s1 = new Student();
+        System.out.println(s1.name);
+        }
+//tester le constructeur avec paramettre 
+        Student s2 = new Student("Ali");
+        System.out.println(s2.name);
+        }
